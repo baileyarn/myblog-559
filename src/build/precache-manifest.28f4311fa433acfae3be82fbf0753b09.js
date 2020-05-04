ab5880cbfdb4a896ea0395bbf7bc1b34e452a01a
baileyarn <bailey.arn17@gmail.com>
2020-05-04 16:58:26 -0500

    First Commit

---
+++ src/build/precache-manifest.28f4311fa433acfae3be82fbf0753b09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbb0ca9c7407e4a1cf30",
+    "url": "/static/css/main.1b89e23e.chunk.css"
+  },
+  {
+    "revision": "bbb0ca9c7407e4a1cf30",
+    "url": "/static/js/main.360ae235.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "7dbf77ab9dd6015ee843cfb92db201b8",
+    "url": "/index.html"
+  }
+];
